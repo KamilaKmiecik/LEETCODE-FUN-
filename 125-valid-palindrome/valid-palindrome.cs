293a5491f467c2984a10b8e176ede579e850168c
Kamila <49212490+KamilaKmiecik@users.noreply.github.com>
2025-07-07 19:55:59 +0200

    Time: 27 ms (34.00%) | Memory: 46.1 MB (35.35%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cs
+++ 125-valid-palindrome/valid-palindrome.cs
@@ -4,9 +4,6 @@ public class Solution {
         //s = s.Where(x => char.IsLetterOrDigit(x)).ToString().ToLower();
         //return s.Equals(s.Reverse());
 
-        if(string.IsNullOrWhiteSpace(s))
-            return true; 
-
         s = new string(s.Where(x => char.IsLetterOrDigit(x)).ToArray()).ToLower();
         int i = 0, l = s.Length -1; 
         
