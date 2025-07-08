53098dd64d75fd00105da877e00a228d76caeba5
Kamila <49212490+KamilaKmiecik@users.noreply.github.com>
2025-07-08 19:13:43 +0200

    Time: 22 ms (50.21%) | Memory: 75.7 MB (14.50%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        HashSet<int> hash = new HashSet<int>();
+
+        for(int i = 0; i < nums.Length; i++)
+            if(hash.Contains(nums[i]))
+                return true;
+            else
+                hash.Add(nums[i]);
+
+        return false; 
+
+        //return nums.Length != new HashSet<int>(nums).Count;
+    }
+}
